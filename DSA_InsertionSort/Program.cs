e5fcc861da77d78272d0b6c26d85ba6c377b9bf5
harpreetsinghwalia <harpreetsinghwalia@gmail.com>
2025-04-16 16:25:38 -0500

    Add project files.

---
+++ DSA_InsertionSort/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Selection sort 
+
+int[] array = { 3, 4, 6, 7, 9, 10 };
+
+ for(int i=0;i<array.Length-1;i++)
+{
+    Console.WriteLine("outer");
+    for(int j=i+1;j>0;j--)
+    {
+        Console.WriteLine("inner");
+        if (array[j] < array[j - 1])
+        {
+            int temp = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = temp;
+
+        }
+        else
+            break;
+    }
+}
+array.ToList().ForEach(x=>Console.WriteLine(x));
